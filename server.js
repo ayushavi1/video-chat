9c5f0d27136f5b011a6dcff686ae9fc54384f63d
lolzone13 <mohitscience123@gmail.com>
2022-06-16 19:39:27 +0530

    added authentication with google OAuth

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
+const colors = require('colors');
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
 const mongoose = require("mongoose")
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const dotenv = require('dotenv');
-dotenv.config({ path: './config/config.env' });
-const Room = require("./models/room")
+const session = require('express-session');
 
-
-const connectDB = require('./config/db');
-connectDB();
+dotenv.config({ path: './config.env' });
 
 
 app.set("view engine", "ejs");
@@ -18,10 +18,33 @@ const io = require("socket.io")(server, {
     origin: '*'
   }
 });
+// const { ExpressPeerServer } = require("peer");
+// const peerServer = ExpressPeerServer(server, {
 
+//   debug: true,
+// });
+
+// app.use("/peerjs", peerServer);
 app.use(express.static("public"));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    maxAge: 24 * 60 * 60 * 1000
+  })
+);
+mongoose.connect("mongodb://localhost:27017/roomDB")
 
+const roomSchema = new mongoose.Schema({
+  roomId: String,
+  users: [String]
+})
 
+const Room = mongoose.model("Room", roomSchema);
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
 
 app.get("/", (req, res) => {
   const roomId = uuidv4();
@@ -33,8 +56,75 @@ app.get("/", (req, res) => {
   room.save();
 });
 
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+
+  return done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  return done(null, user);
+})
+
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "/auth/google/callback"
+},
+  function (accessToken, refreshToken, profile, cb) {
+    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+    //console.log(profile);
+    //console.log(profile);
+    return cb(null, profile);
+  }
+));
+
+app.get('/auth/login', (req, res) => {
+  res.render("login");
+})
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('http://localhost:3000/home');
+  });
+
+app.get('/auth/logout', (req, res) => {
+  try {
+    if (req.user) {
+      req.logout();
+      res.send('Success');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+app.get("/home", (req, res) => {
+  if (req.user) {
+    res.render("home");
+  }
+  else {
+    res.redirect('/auth/login');
+  }
+
+});
+
 app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
+  if (req.user) {
+    res.render("room", { roomId: req.params.room });
+  }
+  else {
+    res.redirect('/auth/login');
+  }
+
 });
 
 io.on("connection", (socket) => {
@@ -47,11 +137,15 @@ io.on("connection", (socket) => {
         foundRoom.save();
       }
     })
-})
+
+
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
+    });
+  });
 });
 
-
-    
 server.listen(process.env.PORT || 3000, function () {
-  console.log("running server at port 3000")
+  console.log(`Server running on port ${process.env.PORT}`.rainbow.bold);
 })
+
